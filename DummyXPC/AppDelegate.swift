bcdd1a3ea9309d938b895d4a86f6473e67a9e3da
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-09-09 14:22:01 +0300

    Initial Commit

---
+++ DummyXPC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DummyXPC
+//
+//  Created by Руслан on 09.09.15.
+//  Copyright (c) 2015 MIPT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DummyXPC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DummyXPC
+//
+//  Created by Руслан on 09.09.15.
+//  Copyright (c) 2015 MIPT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
